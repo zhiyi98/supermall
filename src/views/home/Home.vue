ba10e47da35f7f1ed4051176ef41bd468590b1af
zhiyi98 <742801893@qq.com>
2020-11-28 16:30:06 +0800

    更新

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,13 +3,61 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners" />
     <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control :titles="['流行','新款','精选']" class="tab-control" />
+
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+      <li>列表20</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
 
 import { getHomeMultidata } from "network/home";
 
@@ -19,6 +67,8 @@ export default {
     NavBar,
     HomeSwiper,
     RecommendView,
+    FeatureView,
+    TabControl
   },
   data() {
     return {
@@ -38,8 +88,23 @@ export default {
 </script>
 
 <style scroped>
+#home {
+  padding-top: 44px;
+}
+
 .home-nav {
   background-color: #ff8e96;
   color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
